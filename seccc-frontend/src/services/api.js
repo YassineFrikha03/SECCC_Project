c016dc78bcc514526df348b08e489755f95a283a
Yassine Frikha <yassinefrikha0@gmail.com>
2026-09-09 13:42:44 +0100

    feat: initialize axios instance with production backend base URL

--- seccc-frontend/src/services/api.js
+++ seccc-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api', // L'adresse de ton backend Node.js (Production sur Render)
+  baseURL: 'https://seccc-project.onrender.com/api', // L'adresse de ton backend Node.js (Production sur Render)
 });
 
 export default api;
